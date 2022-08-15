8a64457b0310b6e1bf31c0f4c486c938d53ed19e
icarrington <isaiah.carrington@uconn.edu>
2022-08-15 15:46:09 -0400

    Add comments

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,7 @@ const startButton = document.getElementById('start');
 const scoreDisplay = document.getElementById('score');
 let isStarted = false;
 let score = 0;
-let interval;
+let interval; //The interval at which blocks appear.
 
 function createKey(letter) {
   let key = document.createElement('DIV');
@@ -12,11 +12,13 @@ function createKey(letter) {
   key.classList.add(letter);
   key.innerHTML = letter;
   document.body.append(key);
-  setTimeout(() => key.style.bottom = '110%', 50);
-  setTimeout(() => key.remove(), 3000);
+  
+  setTimeout(() => key.style.bottom = '110%', 50); //This timeout is necessary to change the style immediately. It moves the key from the bottom to the top.
+  setTimeout(() => key.remove(), 3000); //This timeout removes the key after 3 seconds to clean up memory.
 }
 const letters = ['Q', 'W', 'O', 'P'];
 
+//Randomly pick a letter to add to the screen.
 function game() {
   const i = Math.floor(Math.random() * letters.length);
   createKey(letters[i]);
@@ -46,6 +48,7 @@ function handleKeyUp(event) {
   allKeys.forEach(key => {
     const keyCoords= key.getBoundingClientRect();
 
+    //if key is inside the green bar.
     if ((keyCoords.top + 75 < barCoords.bottom) && (keyCoords.top + 75 > barCoords.top)) {
 
       if(key.classList.contains(keyPressed)) {
